75f7390f7fe424331bc9da05d5650c5e541b3428
alceo <alceo@alceo.ru>
2016-05-24 17:43:50 +0300

    add article create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,21 @@ end
 
 def create
   #render plain:  params[:articles].inspect
+@article = Article.new(article_params)
+
+if @article.valid?
+ @article.save
+else
+  render action: 'new'
 end
+
+end
+
+private
+def article_params
+    params.require(:articles).permit(:title,:text) 
+end
+
 
 
 end
