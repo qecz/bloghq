696f4253e888d54fa2eee6bd821b12721bfc87e8
alceo <alceo@alceo.ru>
2016-06-29 17:08:23 +0300

    add username column + index

---
+++ db/migrate/20160629135249_add_username.rb
@@ -0,0 +1,11 @@
+class AddUsername < ActiveRecord::Migration
+  def change
+  add_column :users, :username, :string
+
+
+  end
+
+
+
+
+end
---
+++ db/migrate/20160629140747_add_usernameindex.rb
@@ -0,0 +1,5 @@
+class AddUsernameindex < ActiveRecord::Migration
+  def change
+    add_index :users, :username,                unique: true
+  end
+end
